0aa860896d97c77983ca733190f75a1551144270
Aaron <aaronbaut95@gmail.com>
2022-04-25 19:04:20 -0700

    add variables to store computerPlay and playerSelection variables
    Add variables so that the answers can be compared
    in else if statements

--- java.js
+++ java.js
@@ -2,15 +2,19 @@
 
 let hand = ['rock', 'paper', 'scissor'];
 function computerPlay() {
-   let value = hand[Math.floor(Math.random() * hand.length)];
-   return value;
+  let value = hand[Math.floor(Math.random() * hand.length)];
+  return value;
 }
+let computerAnswer = hand
+let response = function playerSelection() {
+  prompt('rock, paper or scissor?', '')
+}
+let playerSelection = response
 
-function playerSelection() {
-    prompt('rock, paper or scissor?', '')
-}
+let playerAnswer = playerSelection.toLowerCase()
 
 function start() {
-    playerSelection();
-    computerPlay();
+  playerSelection();
+  computerPlay();
+    if playerSelection.toLowerCase() === 'rock' && computerPlay
 }

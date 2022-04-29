b51dd9870b00ebb95410b9a089cdc49c7e7cc08b
Aaron <aaronbaut95@gmail.com>
2022-04-29 10:03:01 -0700

    Add playRound function and change alert to console.log
    Edit last code to console.log result of game instead
    of alert. No longer prompts user for result, instead
    it is input into text itself.

--- java.js
+++ java.js
@@ -6,6 +6,30 @@ function computerPlay() {
   return value;
 }
 
+function playRound(playerAnswer, computerAnswer) {
+    if (playerAnswer === 'rock' && computerAnswer === 'rock' ||
+        playerAnswer === 'paper' && computerAnswer === 'paper' ||
+        playerAnswer === 'scissor' && computerAnswer === 'scissor')
+        {console.log(`${playerAnswer} equals ${computerAnswer}. It's a DRAW!`)
+}   else if (playerAnswer === 'rock' && computerAnswer === 'scissor' ||
+        playerAnswer === 'paper' && computerAnswer === 'rock' ||
+        playerAnswer === 'scissor' && computerAnswer === 'paper')
+    {console.log(`${playerAnswer} beats ${computerAnswer}. You win!`)
+}   else if (playerAnswer === 'rock' && computerAnswer === 'paper' ||
+        playerAnswer === 'paper' && computerAnswer === 'scissor' ||
+        playerAnswer === 'scissor' && computerAnswer === 'rock')
+        {console.log(`${playerAnswer} loses to ${computerAnswer}. You lose`)
+}    else if (playerAnswer === ' ' || playerAnswer === 'null' || playerAnswer === 'undefined')
+        {console.log('field empty')
+}
+};
+let computerAnswer = computerPlay();
+let playerAnswer = 'rock'
+    playerAnswer.toLowerCase();
+
+    playRound(playerAnswer, computerAnswer);
+
+
 
 
 // function playerSelection() {

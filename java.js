0d690802690103c8241160b3fb752ee366bfd205
Aaron <aaronbaut95@gmail.com>
2022-06-15 21:12:10 -0700

    Change event listener to e.target.id, adjust console.log results
    return button.id function returns all three buttons instead
    of the one clicked. e.target.id fixes that issue.
    
    Change console.log functions to return text and attempt to
    display it in the DOM

--- java.js
+++ java.js
@@ -1,40 +1,53 @@
-//Make a function computerPlay to randomly ccomputerSelectionhoose rock paper or scissors
+//Make a function computerPlay to randomly choose rock paper or scissors
 
 let hand = ['rock', 'paper', 'scissor'];
-function computerPlay() {
-  let value = hand[Math.floor(Math.random() * hand.length)];
-  return value;
-}
+    function computerPlay() {
+        let value = hand[Math.floor(Math.random() * hand.length)];
+        console.log("Computer selects",value)
+        return value;
+    }
+
 
 const buttons = document.querySelectorAll('button');
-    let playerSelection = buttons.forEach((button) => {
-        button.addEventListener('click', () => {
-            computerPlay();
-            playRound();
-            return button.id;
+    buttons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+           let userSelection = e.target.id
+            let computerSelection = computerPlay();
+            playRound(userSelection,computerSelection);
+        
+            });
         });
-    });
-   
 
-function playRound() {
-    
-    if (playerSelection === 'rock' && computerPlay === 'rock' ||
-        playerSelection === 'paper' && computerPlay === 'paper' ||
-        playerSelection === 'scissor' && computerPlay === 'scissor')
-        {console.log(`${playerSelection} equals ${computerPlay}. It's a DRAW!`)
-}   else if (playerSelection === 'rock' && computerPlay === 'scissor' ||
-        playerSelection === 'paper' && computerPlay === 'rock' ||
-        playerSelection === 'scissor' && computerPlay === 'paper')
-    {console.log(`${playerSelection} beats ${computerPlay}. You win!`)
-}   else if (playerSelection === 'rock' && computerPlay === 'paper' ||
-        playerSelection === 'paper' && computerPlay === 'scissor' ||
-        playerSelection === 'scissor' && computerPlay === 'rock')
-        {console.log(`${playerSelection} loses to ${computerPlay}. You lose`)
-}    else if (playerSelection === ' ' || playerSelection === 'null' || playerSelection === 'undefined')
-        {console.log('field empty')
+function playRound(userSelection,computerSelection) {
+    let outcome;
+    if (userSelection === 'rock' && computerSelection === 'rock' ||
+        userSelection === 'paper' && computerSelection === 'paper' ||
+        userSelection === 'scissor' && computerSelection === 'scissor')
+        {outcome = `${userSelection} equals ${computerSelection}. It's a DRAW!`
+}   else if (userSelection === 'rock' && computerSelection === 'scissor' ||
+        userSelection === 'paper' && computerSelection === 'rock' ||
+        userSelection === 'scissor' && computerSelection === 'paper')
+    {outcome = `${userSelection} beats ${computerSelection}. You win!`
+}   else if (userSelection === 'rock' && computerSelection === 'paper' ||
+        userSelection === 'paper' && computerSelection === 'scissor' ||
+        userSelection === 'scissor' && computerSelection === 'rock')
+        {outcome = `${userSelection} loses to ${computerSelection}. You lose`
+}    else if (userSelection === ' ' || userSelection === 'null' || userSelection === 'undefined')
+        {outcome = 'field empty'
         
 }
+    return outcome;
 };
+
+let playResult = playRound();
+
+const content = document.querySelector('body')
+const results = document.createElement('div')
+results.classList.add('results')
+results.innerHTML = '${playResult}';
+
+content.appendChild(results);
+
 
     
 // function game() {

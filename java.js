e839d189ed1482bdf31e1d397b1dbeaa192fb3c3
Aaron <aaronbaut95@gmail.com>
2022-06-19 09:43:51 -0700

    Add score div and clean up js file
    Add score div for future display of score.
    Delete code that was from previous lesson

--- java.js
+++ java.js
@@ -31,53 +31,15 @@ function playRound(userSelection,computerSelection) {
         userSelection === 'paper' && computerSelection === 'scissor' ||
         userSelection === 'scissor' && computerSelection === 'rock')
         {results.innerText = `${userSelection} loses to ${computerSelection}. You lose`
-        
+
+};
 };
 
 
-const content = document.querySelector('body')
-const results = document.createElement('div')
-results.classList.add('results')
-
+const content = document.querySelector('body');
+const results = document.createElement('div');
+results.classList.add('results');
 content.appendChild(results);
-
-
-    
-// function game() {
-
-//     let computerPlay = computerPlay();
-//     let playerSelection = prompt('rock, paper or scissor?', '')
-//     playRound(playerSelection, computerPlay);
-//         console.log(`Round ${i} out of 5`)
-//     }
-
-// game();
-
-
-// function playerSelection() {
-//   let Selection = prompt('rock, paper or scissor?', '')
-//   return Selection;
-// }
-
-// function playRound() {
-//     let playerSelection = playerSelection();;
-//     let computerPlay = `${computerPlay()}`
-//     if (playerSelection === 'rock' && computerPlay === 'rock' ||
-//         playerSelection === 'paper' && computerPlay === 'paper' ||
-//         playerSelection === 'scissor' && computerPlay === 'scissor')
-//         {alert(`${playerSelection} equals ${computerPlay}. It's a DRAW!`)
-// }   else if (playerSelection === 'rock' && computerPlay === 'scissor' ||
-//         playerSelection === 'paper' && computerPlay === 'rock' ||
-//         playerSelection === 'scissor' && computerPlay === 'paper')
-//     {alert(`${playerSelection} beats ${computerPlay}. You win!`)
-// }   else if (playerSelection === 'rock' && computerPlay === 'paper' ||
-//         playerSelection === 'paper' && computerPlay === 'scissor' ||
-//         playerSelection === 'scissor' && computerPlay === 'rock')
-//         {alert(`${playerSelection} loses to ${computerPlay}. You lose`)
-// }    else if (playerSelection === ' ' || playerSelection === 'null' || playerSelection === 'undefined')
-//         {alert('field empty')
-
-        
-// }
-// };
-
+const score = document.createElement('div');
+score.classList.add('score');
+content.appendChild(score);
